5b52228dc5147cab1ee2c185003ee8e810773b41
root <root@DESKTOP-9GHTC3O.localdomain>
2023-12-17 09:50:13 -0800

    v1.0

---
+++ resources/views/pages/contact-sales-register.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact Sales Register Page</title>
+    @vite('resources/css/app.css')
+    @vite('resources/css/glass.css')
+</head>
+<body class="w-full h-screen bg-white">
+
+    <section class="bg-gradient-to-tr from-rose-100 via-pink-200 to-indigo-400 w-full h-screen">
+
+            <form action="" method="post" class="grid grid-cols-2 glass w-full h-screen items-center justify-center col-span-2 space-y-8">
+                    <label for="first_name" class="font-semibold">
+                        First name
+                        <input
+                            type="text"
+                            name="first_name"
+                            class="rounded-md bg-white w-full px-3 py-2 outline-none
+                        appearance-none mt-4 focus:ring-2 focus:ring-pink-300
+                        transition-all duration-700">
+                    </label>
+            </form>
+
+    </section>
+
+</body>
+</html>
